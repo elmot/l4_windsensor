a14c65ac1d5a32359fe0b5eaeda12bba6389c65d
elmot <me@elmot.xyz>
2017-05-03 01:56:11 +0300

    Protect extra GPIOA pins

--- Src/main.c
+++ Src/main.c
@@ -101,6 +101,10 @@ int main(void)
   /* USER CODE BEGIN 2 */
   __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_UPDATE);
   HAL_TIM_Base_Start(&htim2);
+  HAL_GPIO_LockPin(GPIOA, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
+                          GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
+                          GPIO_PIN_10| GPIO_PIN_11| GPIO_PIN_12| GPIO_PIN_13|
+                          GPIO_PIN_14| GPIO_PIN_15);
 
   /* USER CODE END 2 */
 
